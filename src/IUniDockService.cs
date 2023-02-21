7a22fe30670026612caa003b379a92260f6c3a32
npolyak <nick.polyak@verizon.net>
2023-02-20 21:27:41 -0500

    added requested functionality

--- src/IUniDockService.cs
+++ src/IUniDockService.cs
@@ -14,13 +14,20 @@ namespace NP.Avalonia.UniDockService
         ObservableCollection<DockItemViewModelBase> DockItemsViewModels { get; set; }
 
         void SaveToFile(string filePath);
+        void SaveDockManagerParamsToStream(Stream stream);
 
         void RestoreFromFile(string filePath,
                              bool restorePredefinedWindowsPositionParams = false);
+        void RestoreDockManagerParamsFromStream(Stream stream,
+                                                bool restorePredefinedWindowsPositionParams = false);
 
         void SaveViewModelsToFile(string filePath);
 
+        void SaveViewModelsToStream(Stream stream);
+
         void RestoreViewModelsFromFile(string filePath, params Type[] extraTypes);
+
+        void RestoreViewModelsFromStream(Stream stream, params Type[] extraTypes);
 
         DockObjectInfo? GetParentGroupInfo(string? dockId);
 
