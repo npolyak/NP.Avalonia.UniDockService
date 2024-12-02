9705696e8667b32ea7469cbd46d5835afaa3de5c
npolyak <nick.polyak@verizon.net>
2024-12-02 15:11:17 -0500

    fixed saving/restoring DMA layout

---
+++ src/IUniDockServiceContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace NP.Ava.UniDockService
+{
+    public interface IUniDockServiceContainer
+    {
+        IUniDockService DockService { get; }
+
+        void SetNewDockService();
+
+        void RestoreLayout();
+    }
+}
